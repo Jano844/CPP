44da09e8de49ff59fb9701ff7ddac636e5d54696
Jano844 <jani.saenger@gmail.com>
2024-01-29 12:25:24 +0100

    Module00 ex00 and 01 done, starting ex02

---
+++ Module00/ex01/contact.cpp
@@ -0,0 +1,42 @@
+
+#include "contact.hpp"
+
+void	contact::set_first_name(std::string first_name) {
+	this->first_name = first_name;
+}
+
+void	contact::set_last_name(std::string last_name) {
+	this->last_name = last_name;
+}
+
+void	contact::set_nick_name(std::string nick_name) {
+	this->nick_name = nick_name;
+}
+
+void	contact::set_phone_number(std::string phone_number) {
+	this->phone_number = phone_number;
+}
+
+void	contact::set_darkest_secret(std::string darkest_secret) {
+	this->darkest_secret = darkest_secret;
+}
+
+std::string	contact::get_first_name(void) {
+	return (this->first_name);
+}
+
+std::string	contact::get_last_name(void) {
+	return (this->last_name);
+}
+
+std::string	contact::get_nick_name(void) {
+	return (this->nick_name);
+}
+
+std::string	contact::get_phone_number(void) {
+	return (this->phone_number);
+}
+
+std::string	contact::get_darkest_secret(void) {
+	return (this->darkest_secret);
+}
