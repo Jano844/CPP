58cdb2fc5b6f6119abbfa14ba25a8181ff01c920
Jan Sanger <jsanger@2-D-6.42heilbronn.de>
2024-02-06 21:46:21 +0100

    hs

---
+++ Module02/ex03/include/Point.hpp
@@ -0,0 +1,22 @@
+
+
+#pragma once
+
+#include "Fixed.hpp"
+#include <cmath>
+
+class Point
+{
+private:
+	const Fixed	x;
+	const Fixed	y;
+public:
+	Point();
+	Point(const float num1, const float num2);
+	Point(const Point &reference);
+	Point& operator=(const Point &other);
+	~Point();
+
+	Fixed get_x() const;
+	Fixed get_y() const;
+};
---
+++ Module02/ex03/srcs/main.cpp
@@ -0,0 +1,14 @@
+
+
+#include "../include/Fixed.hpp"
+#include "../include/Point.hpp"
+
+int main()
+{
+	Point	a(1, 5);
+	Point	b(12.5f, 10.6f);
+	Point	c;
+
+	std::cout << 
+	return (0);
+}
