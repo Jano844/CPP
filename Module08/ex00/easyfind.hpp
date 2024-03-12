12faf8eb3d48f30d5b6b1df5a488f609d789d0b1
Jan Sanger <jsanger@2-D-4.42heilbronn.de>
2024-03-12 21:22:40 +0100

    M08 ex00 Done

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -3,8 +3,15 @@
 #pragma once
 
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 template <typename T>
-int easyfind(T container, unsigned int toFind) {
-	return (0);
+int easyfind(T & container, unsigned int toFind) {
+	if (std::find( container.begin(), container.end(), toFind ) != container.end()) {
+		std::cout << "Found\n";
+		return 0;
+	}
+	std::cout << "not Found\n";
+	return 1;
 };
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -2,3 +2,16 @@
 
 #include "easyfind.hpp"
 
+int main()
+{
+	std::vector<int> test;
+	test.push_back(1);
+	test.push_back(2);
+	test.push_back(3);
+	test.push_back(4);
+	test.push_back(5);
+
+
+	easyfind(test, 5);
+	return 0;
+}
