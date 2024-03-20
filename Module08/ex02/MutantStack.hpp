21191b418a0150cc65737ea338d1f1271fcbe3db
Jan Sanger <jsanger@2-D-4.42heilbronn.de>
2024-03-20 15:13:58 +0100

    Done with CPP08

--- Module08/ex02/MutantStack.hpp
+++ Module08/ex02/MutantStack.hpp
@@ -9,7 +9,35 @@
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-private:
-
 public:
+	MutantStack() : std::stack<T>() {}
+	MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+	MutantStack &operator=(const MutantStack &other)
+	{
+		if (this != &other)
+			std::stack<T>::operator=(other);
+		return *this;
+	}
+	~MutantStack() {}
+
+	typedef typename std::stack<T>::container_type::iterator iterator; // define iterator as a type of the stack's container's iterator
+	iterator begin() { return std::stack<T>::c.begin(); } // return iterator to the beginning of the stack
+	iterator end() { return std::stack<T>::c.end(); } // return iterator to the end of the stack
+
+
+	// Iterating through the stack and use function on each element
+	void iterate(void (*f)(T &))
+	{
+		iterator it = begin();
+		iterator ite = end();
+		while (it != ite)
+			f(*it++);
+	}
+
+	// print function
 };
+
+template <typename T>
+void print(T t) {
+	std::cout << t << std::endl;
+}
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -1,8 +1,91 @@
 
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+#include <cstdlib>
+#include <ctime>
 
 int main()
 {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	mstack.push(21);
+	std::cout << mstack.top() << std::endl;
+
+	mstack.pop();
+	std::cout << mstack.top() << std::endl;
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	mstack.push(0);
+
+	std::cout << "Iterating through the stack" << std::endl;
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	--it;
+	while (++it != ite)
+		std::cout << *it << std::endl;
+
 	return 0;
 }
+
+
+
+
+
+
+
+// int main()
+// {
+// 	std::list<int> mstack;
+// 	mstack.push_back(5);
+// 	mstack.push_back(17);
+// 	mstack.push_back(21);
+
+// 	std::cout << mstack.back() << std::endl;
+// 	// std::cout << *(--(mstack.end())) << std::endl; //lol didnt know about .back, but it's the same as the line above
+
+// 	mstack.pop_back();
+// 	std::cout << mstack.back() << std::endl;
+// 	std::cout << mstack.size() << std::endl;
+
+// 	mstack.push_back(3);
+// 	mstack.push_back(5);
+// 	mstack.push_back(737);
+// 	mstack.push_back(0);
+
+
+// 	std::cout << "Iterating through the stack" << std::endl;
+
+// 	std::list<int>::iterator it = mstack.begin();
+// 	std::list<int>::iterator ite = mstack.end();
+// 	--it;
+// 	while (++it != ite)
+// 		std::cout << *it << std::endl;
+
+// 	return 0;
+// }
+
+
+
+
+// int main()
+// {
+
+// 	MutantStack<int> mstack;
+
+// 	srand(time(0));
+// 	for (int i = 0; i < 1000; i++)
+// 		mstack.push(rand() % 100);
+	
+// 	MutantStack<int> test(mstack);
+// 	// MutantStack<int> test;
+// 	// test = mstack;
+
+// 	test.iterate(::print);
+// }
