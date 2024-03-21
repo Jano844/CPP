a3481dc43e694a280ddd1744dddacf441b7a3543
Jan Sanger <jsanger@3-C-3.42heilbronn.de>
2024-03-21 14:57:01 +0100

    Starting M09

---
+++ Module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class BitcoinExchange
+{
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+	void run();
+private:
+};
---
+++ Module09/ex00/main.cpp
@@ -0,0 +1,15 @@
+
+
+
+#include "BitcoinExchange.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Wrog Input!" << std::endl;
+		return 1;
+	}
+	
+	return 0;
+}
